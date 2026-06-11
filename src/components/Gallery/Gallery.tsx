a6f524da276dd0f26da61324410a9bc7d961865d
котуша <xomnrt@MBP-kotusa.lan>
2026-06-11 21:04:31 +0300

    add gallery prototype

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -2,7 +2,10 @@ import s from "./Gallery.module.scss"
 
 function GalleryItem() {
     return (
-    <article className={s.galleryItem}></article>
+    <article className={s.galleryItem}>
+        <h2 className={s.city}>city</h2>
+        <h2 className={s.year}>2xxx</h2>
+    </article>
     )
 }
 
@@ -14,8 +17,6 @@ function Gallery() {
         <GalleryItem />
         <GalleryItem />
         <GalleryItem />
-        <GalleryItem />
-
     </div>
     )
 }
